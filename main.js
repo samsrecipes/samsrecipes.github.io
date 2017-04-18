42305055c942309f75c554d4a7501c5d5e639baa
samsrecipes <iamsam91@comcast.net>
2017-04-18 16:49:11 -0400

    Test 4

--- main.js
+++ main.js
@@ -1,73 +1,20 @@
-$(document).ready(function() {
-	$('#appetizer-nav').click(function(e) {
-		e.preventDefault;
-		hideAllRecipes();
-		$('.appetizer').show();
-	});
 
-	$('#bread-nav').click(function(e) {
-		e.preventDefault;
-		hideAllRecipes();
-		$('.bread').show();
-	});
 
-	$('#breakfast-nav').click(function(e) {
-		e.preventDefault;
-		hideAllRecipes();
-		$('.breakfast').show();
-	});
+jQuery(document).ready(function() {
 
-	$('#dessert-nav').click(function(e) {
-		e.preventDefault;
-		hideAllRecipes();
-		$('.dessert').show();
-	});
+	// read JSON file for list of recipes
+    $.getJSON('recipes.json', function(data) {
 
-	$('#dinner-nav').click(function(e) {
-		e.preventDefault;
-		hideAllRecipes();
-		$('.dinner').show();
-	});
+    	var recipeList = [];
 
-	$('#smoothie-nav').click(function(e) {
-		e.preventDefault;
-		hideAllRecipes();
-		$('.smoothie').show();
-	});
+    	// loop through list of recipes
+    	$.each(data.recipes, function(key, recipe) {
+    		recipeList.push('<div><h3>' + recipe.title + '</h3></div>');
+    	});
 
-	$('#snack-nav').click(function(e) {
-		e.preventDefault;
-		hideAllRecipes();
-		$('.snack').show();
-	});
+    	// add all recipes to main section
+    	$('.recipe-block').html(recipeList.join(''));
 
-	$('#soup-nav').click(function(e) {
-		e.preventDefault;
-		hideAllRecipes();
-		$('.soup').show();
-	});
-
-	$('#all-nav').click(function(e) {
-		e.preventDefault;
-		$('.recipe-item').show();
-	});
-
-	$('.nav-sidebar li').click(function(e) {
-		e.preventDefault();
-		removeActive();
-		$(this).addClass('active');
-	});
-
+    });
 
 });
-
-
-function removeActive() {
-	$('.nav-sidebar li').removeClass('active');
-}
-
-function hideAllRecipes() {
-	$('.recipe-item').hide()
-}
-
-
