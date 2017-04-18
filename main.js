8ade6b258dd17b2dab2441699e5dcd7c03689d03
samsrecipes <iamsam91@comcast.net>
2017-04-18 17:09:49 -0400

    Test 5

--- main.js
+++ main.js
@@ -9,7 +9,11 @@ jQuery(document).ready(function() {
 
     	// loop through list of recipes
     	$.each(data.recipes, function(key, recipe) {
-    		recipeList.push('<div><h3>' + recipe.title + '</h3></div>');
+    		recipeList.push('<div><h3>' + recipe.title + '</h3>');
+    		for (var i in recipes.categories) {
+    			recipeList.push('<p>' + recipes.categories[i] + '</p>');
+    		}
+    		recipeList.push('<p>serves: ' + recipe.serves + '</p></div>');
     	});
 
     	// add all recipes to main section
