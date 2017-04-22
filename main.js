4b24ebeb118929dadee55d1c714cc11744db12f3
samsrecipes <iamsam91@comcast.net>
2017-04-22 13:02:32 -0400

    Test23

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ jQuery(document).ready(function() {
     		for (var i in recipe.categories) {
     			recipeList.push(recipe.categories[i] + ' ');
     		}
-    		recipeList.push('"><h3>' + recipe.title + '</h3><p>serves: ' + recipe.serves + '</p><p>prep: ' + recipe.prep + '</p><p>wait: ' + recipe.wait + '</p><button type="button" class="btn btn-default" data-toggle="modal" data-designation="' + recipe.designation + '" data-target="#recipe-modal">View Recipe</button></div>');
+    		recipeList.push('"><div class="panel panel-default"><div class="panel-heading">' + recipe.title + '</div><div class="panel-body"><p>serves: ' + recipe.serves + '</p><p>prep: ' + recipe.prep + '</p><p>wait: ' + recipe.wait + '</p><button type="button" class="btn btn-default" data-toggle="modal" data-designation="' + recipe.designation + '" data-target="#recipe-modal">View Recipe</button></div></div></div>');
     	});
 
     	// add all recipes to main section
